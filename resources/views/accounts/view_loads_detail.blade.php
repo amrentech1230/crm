387dc57e9134dc792e4cc936312271932a1f3226
Neha Dhiman <neha@geeshasolutions.com>
2026-07-20 13:14:52 -0400

    view credit

--- resources/views/accounts/view_loads_detail.blade.php
+++ resources/views/accounts/view_loads_detail.blade.php
@@ -74,7 +74,6 @@
                             </div>
                         </div>
 
-                        <!-- Divider -->
                         <hr class="my-4">
 
                         <!-- Logs Section -->
